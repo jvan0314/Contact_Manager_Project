3bc61d3d5d799a8030786105f7c2b7da37b256ed
Jenfrey Van <jvan0314@gmail.com>
2018-07-23 21:25:51 -0400

    updated app title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Evolent Health Contact Manager';
+  title = 'Contact Manager';
 }
